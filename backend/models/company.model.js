70cf468cbc3449f2509e1459fe0ce9b0e1f13a2f
Gautam <gohelgautam2004@gmail.com>
2024-12-25 14:43:19 +0530

    add application, company, and job models; update user model with profile enhancements

---
+++ backend/models/company.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  logo: {
+    type: String,//cloudinary image url
+    default: "",
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  }
+},{timestamps:true});
+
+export const Company = mongoose.model("Company", companySchema);
